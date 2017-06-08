3f0ad9222ebf6f00b9228763c7268ac253fbd737
sj1026209745 <jsmeli@163.com>
2017-06-08 15:38:31 +0800

    commit 0.0.2

---
+++ permissioncheck/build/generated/source/buildConfig/release/com/jsmeli/permissioncheck/sjml/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.jsmeli.permissioncheck.sjml;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.jsmeli.permissioncheck.sjml";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "0.0.2";
+}
